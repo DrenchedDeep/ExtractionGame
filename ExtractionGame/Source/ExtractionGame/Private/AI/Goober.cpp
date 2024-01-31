15bbd644514695cf776e4161314699682891fd93
theprince1701 <96841021+theprince1701@users.noreply.github.com>
2024-01-30 20:47:32 -0500

    killing goobers

--- ExtractionGame/Source/ExtractionGame/Private/AI/Goober.cpp
+++ ExtractionGame/Source/ExtractionGame/Private/AI/Goober.cpp
@@ -3,3 +3,38 @@
 
 #include "AI/Goober.h"
 
+#include "Net/UnrealNetwork.h"
+
+void AGoober::ApplyDamage(float Damage)
+{
+	CurrentHealth -= Damage;
+
+	if(CurrentHealth <= 0)
+	{
+		CurrentHealth = 0;
+		bIsDead = true;
+		BP_OnGooberDeadServer();
+	}
+}
+
+void AGoober::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
+{
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+
+	DOREPLIFETIME(AGoober, bIsDead);
+}
+
+void AGoober::OnRep_Dead()
+{
+	BP_OnGooberDeadAll();
+}
+
+void AGoober::BeginPlay()
+{
+	Super::BeginPlay();
+
+	if(HasAuthority())
+	{
+		CurrentHealth = 100.f;
+	}
+}
--- ExtractionGame/Source/ExtractionGame/Public/AI/Goober.h
+++ ExtractionGame/Source/ExtractionGame/Public/AI/Goober.h
@@ -5,15 +5,35 @@
 #include "CoreMinimal.h"
 #include "GooberManager.h"
 #include "GooberTask.h"
+#include "Core/CharacterPawnBase.h"
 #include "GameFramework/Actor.h"
 #include "Goober.generated.h"
 
 
 UCLASS()
-class EXTRACTIONGAME_API AGoober : public AActor
+class EXTRACTIONGAME_API AGoober : public ACharacterPawnBase
 {
 	GENERATED_BODY()
 	
 public:
+	UPROPERTY(ReplicatedUsing=OnRep_Dead);
+	bool bIsDead;
 
+	UFUNCTION(BlueprintCallable)
+	void ApplyDamage(float Damage);
+
+protected:
+	virtual void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const override;
+	virtual void BeginPlay() override;
+	
+	UFUNCTION()
+	virtual void OnRep_Dead();
+
+	UFUNCTION(BlueprintImplementableEvent)
+	void BP_OnGooberDeadAll();
+	UFUNCTION(BlueprintImplementableEvent)
+	void BP_OnGooberDeadServer();
+
+private:
+	float CurrentHealth;
 };
